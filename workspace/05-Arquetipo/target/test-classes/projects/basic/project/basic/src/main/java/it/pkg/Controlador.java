9e8f0a68974b807694608ebc5b067b1f432737d1
Victor <victorherrerocazurro@gmail.com>
2018-09-17 20:03:12 +0200

    Dia 1

---
+++ workspace/05-Arquetipo/target/test-classes/projects/basic/project/basic/src/main/java/it/pkg/Controlador.java
@@ -0,0 +1,8 @@
+package it.pkg;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class Controlador {
+
+}
---
+++ workspace/06-ProyectoAPartirDeArquetipoLocal/src/main/java/com/atsistemas/_ProyectoAPartirDeArquetipoLocal/Controlador.java
@@ -0,0 +1,8 @@
+package com.atsistemas._ProyectoAPartirDeArquetipoLocal;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class Controlador {
+
+}
