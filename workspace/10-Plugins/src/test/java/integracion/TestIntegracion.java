98ffc5e5d3d092f7b2d2956a69fb3e7de2fa184c
Victor <victorherrerocazurro@gmail.com>
2018-09-19 20:03:18 +0200

    Plugins

---
+++ workspace/10-Plugins/src/test/java/integracion/TestIntegracion.java
@@ -0,0 +1,12 @@
+package integracion;
+
+import static org.junit.Assert.*;
+
+public class TestIntegracion {
+
+	@org.junit.Test
+	public void test() {
+		//fail("Not yet implemented");
+	}
+
+}
---
+++ workspace/10-Plugins/src/test/java/unitarias/Test.java
@@ -0,0 +1,13 @@
+package unitarias;
+
+import static org.junit.Assert.*;
+
+public class Test {
+
+	@org.junit.Test
+	public void test() {
+		//fail("Not yet implemented");
+		System.out.println("");
+	}
+
+}
